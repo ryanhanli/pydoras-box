3b6f49741e0d7b5bb1424e9f44e16d3101f7adfd
Ryan Li <rl3294@nyu.edu>
2020-07-24 17:02:00 -0400

    Finish Excel part

--- Excel/openpyBasics3.py
+++ Excel/openpyBasics3.py
@@ -48,3 +48,20 @@ wb = openpyxl.load_workbook('produceSales.xlsx')
 sheet = wb.active
 sheet.freeze_panes = 'A2'
 wb.save('freezeExample.xlsx')
+
+# Charts *This is deprecated
+wb = openpyxl.Workbook()
+sheet = wb.active
+for i in range(1,11):	# create some data in column A:
+	sheet['A' + str(i)] = i
+refObj = openpyxl.charts.Reference(sheet, (1, 1), (10, 1))
+seriesObj = openpyxl.charts.Series(refObj, title='First series')
+chartObj = openpyxl.charts.BarChart()
+chartObj.append(seriesObj)
+chartObj.drawing.top = 50 # set the position
+chartObj.drawing.left = 100
+chartObj.drawing.width = 300 # set the size
+chartObj.drawing.height = 200
+
+sheet.add_chart(chartObj)
+wb.save('sampleChart.xlsx')
